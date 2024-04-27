61afdad170cb4be4d68ee5dae8449f87165e9ee7
Rohan Sampath <rohan.s94@gmail.com>
2024-04-27 05:25:16 -0700

    Final updates to constituency guessing game

--- src/ConstituencyGame.tsx
+++ src/ConstituencyGame.tsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { TextInput, Image, Title, Stack, Table } from "@mantine/core";
+import { Box, TextInput, Image, Title, Stack, Table } from "@mantine/core";
 
 interface Constituency {
   "State/UT": string;
   PCName: string;
   "Alternate Spellings": string[];
+}
+
+interface StateVernacular {
+  State: string;
+  Vernacular: string | null;
 }
 
 const ConstituencyGame: React.FC = () => {
@@ -14,17 +19,29 @@ const ConstituencyGame: React.FC = () => {
   }>({});
   const [correctCount, setCorrectCount] = useState(0);
   const [data, setData] = useState<Constituency[]>([]);
+  const [stateVernacularMap, setStateVernacularMap] = useState<{
+    [state: string]: string | null;
+  }>({});
 
   const formatName = (name: string) => {
     return name.toLowerCase().replace(/\band\b|\&|\s+|\-+|\\.+/g, "");
   };
 
   useEffect(() => {
-    fetch("/constituencies_formatted.json")
-      .then((response) => response.json())
-      .then((loadedData) => {
-        const constituencyData = loadedData as Constituency[];
-        setData(constituencyData);
+    Promise.all([
+      fetch("/constituencies_formatted.json"),
+      fetch("/state_to_vernacular.json"),
+    ])
+      .then(([constituenciesResponse, vernacularResponse]) =>
+        Promise.all([constituenciesResponse.json(), vernacularResponse.json()])
+      )
+      .then(([constituencyData, vernacularData]) => {
+        setData(constituencyData as Constituency[]);
+        const vernacularMap: { [state: string]: string | null } = {};
+        (vernacularData as StateVernacular[]).forEach((entry) => {
+          vernacularMap[entry.State] = entry.Vernacular;
+        });
+        setStateVernacularMap(vernacularMap);
       })
       .catch((error) => console.error("Error loading JSON:", error));
   }, []);
@@ -83,7 +100,15 @@ const ConstituencyGame: React.FC = () => {
     .sort(([stateA], [stateB]) => stateA.localeCompare(stateB))
     .map(([state, guesses]) => (
       <Table.Tr key={state}>
-        <Table.Td style={{ width: 200 }}>{state}</Table.Td>
+        <Table.Td style={{ width: 200, maxWidth: 400 }}>
+          {state}
+          {stateVernacularMap[state] && (
+            <>
+              <br />
+              {stateVernacularMap[state]}
+            </>
+          )}
+        </Table.Td>
         <Table.Td style={{ width: 200, textAlign: "center" }}>
           {guesses.length} / {getTotalConstituenciesInState(state)}
         </Table.Td>
@@ -92,42 +117,83 @@ const ConstituencyGame: React.FC = () => {
     ));
 
   return (
-    <Stack
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 50,
-        marginLeft: 50,
-        marginRight: 50,
-        marginBottom: 50,
-      }}
-    >
-      <Image src="/emblem_of_india.svg" alt="Emblem of India" w={100} />
-      <Title order={1} style={{ marginBottom: "1rem" }}>
-        Guess the Lok Sabha Constituencies!
-      </Title>
-      <TextInput
-        size="md"
-        label="Constituency Name (निर्वाचन क्षेत्र का नाम)"
-        placeholder="Start typing..."
-        onChange={handleInputChange}
-        value={input}
+    <Box>
+      <Stack
         style={{
-          marginTop: 20,
-          padding: "10px 20px",
-          fontSize: "1rem",
-          width: "100%",
-          maxWidth: "500px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+
+          marginTop: 50,
+          marginLeft: 50,
+          marginRight: 50,
+          marginBottom: 20,
         }}
-      />
-      <Title order={2}>{correctCount} / 543</Title>
-      <Stack style={{ marginTop: 20 }}>
-        <Table highlightOnHover>
+      >
+        <Image src="/emblem_of_india.svg" alt="Emblem of India" w={100} />
+        <Title order={1} style={{ marginBottom: "1rem" }}>
+          Guess the Lok Sabha Constituencies!
+        </Title>
+      </Stack>
+      <Stack
+        style={{
+          position: "sticky",
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 1,
+          backgroundColor: "white",
+          paddingTop: 20,
+          paddingBottom: 20,
+          paddingLeft: 50,
+          paddingRight: 50,
+          borderBottom: "1px solid #eaeaea",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <TextInput
+          size="md"
+          label="Constituency Name (निर्वाचन क्षेत्र का नाम)"
+          placeholder="Start typing..."
+          onChange={handleInputChange}
+          value={input}
+          style={{
+            marginTop: 20,
+            padding: "10px 20px",
+            fontSize: "1rem",
+            width: "100%",
+            maxWidth: "500px",
+          }}
+        />
+        <Title order={2}>{correctCount} / 543</Title>
+      </Stack>
+      <Box
+        style={{
+          marginTop: 50,
+          marginBottom: 50,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <Table
+          highlightOnHover
+          style={{
+            width: "auto",
+            minWidth: "800px",
+            maxWidth: "100%",
+          }}
+        >
           <Table.Thead>
             <Table.Tr>
-              <Table.Th style={{ width: 200 }}>State/UT</Table.Th>
+              <Table.Th style={{ width: 200, maxWidth: 400 }}>
+                State/UT
+              </Table.Th>
               <Table.Th style={{ width: 200 }}># Correct / # Total</Table.Th>
               <Table.Th style={{ maxWidth: 800 }}>
                 Constituencies Guessed
@@ -136,8 +202,8 @@ const ConstituencyGame: React.FC = () => {
           </Table.Thead>
           <Table.Tbody>{rows}</Table.Tbody>
         </Table>
-      </Stack>
-    </Stack>
+      </Box>
+    </Box>
   );
 };
 
